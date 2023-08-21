04174739a63593ab6f1e639eee62c7b145319d0d
mikiii24 <mikibask24@gmail.com>
2023-08-22 00:51:10 +0900

    質問投稿に関するコントローラーの設定

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,22 @@
 class QuestionsController < ApplicationController
   def index
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:question_title, :question_detail, :qual_id)
+  end
 end
