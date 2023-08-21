0a30af098bdad198782381e3f0508861cd55883f
mikiii24 <mikibask24@gmail.com>
2023-08-21 23:53:41 +0900

    資格モデルの生成と記述

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,9 @@
 class Question < ApplicationRecord
   belongs_to :user
-  
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :qual
+
   with_options presence: true do
     validates :question_title
     validates :question_detail
