b484a9dc9fa76529e9eb43031099d9ce51191fb9
mikiii24 <mikibask24@gmail.com>
2023-08-21 17:10:50 +0900

    ユーザーテーブルの作成

--- db/migrate/20230821055235_devise_create_users.rb
+++ db/migrate/20230821055235_devise_create_users.rb
@@ -4,6 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false, default: ""
+      t.string :name,               null: false, default: ""
+      t.string :name_kana,          null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
