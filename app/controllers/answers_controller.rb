3ae7632f8ad22cd85eada3d6498ad6e3f9e58690
mikiii24 <mikibask24@gmail.com>
2023-08-24 23:08:01 +0900

    answerコントローラーの保存アクションの記述

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,10 @@
 class AnswersController < ApplicationController
+  def create
+    Answer.create(answer_params)
+  end
+
+  private
+  def answer_params
+    params.require(:answer).permit(:answer_text).merge(user_id: current_user.id, question_id: params[:question_id])
+  end
 end
