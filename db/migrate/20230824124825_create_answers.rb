73fb88d1161444f34c0b08e521ac90bf685f9011
mikiii24 <mikibask24@gmail.com>
2023-08-24 22:28:27 +0900

    回答テーブルの作成

--- db/migrate/20230824124825_create_answers.rb
+++ db/migrate/20230824124825_create_answers.rb
@@ -1,6 +1,9 @@
 class CreateAnswers < ActiveRecord::Migration[7.0]
   def change
     create_table :answers do |t|
+      t.integer :user_id
+      t.integer :question_id
+      t.text :answer_text
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_124825) do
+  create_table "answers", charset: "utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "question_id"
+    t.text "answer_text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "questions", charset: "utf8", force: :cascade do |t|
     t.string "question_title", null: false
     t.text "question_detail", null: false
