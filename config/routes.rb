8f7196fb69b6c949b2298c87f25e9146cb07e6b7
mikiii24 <mikibask24@gmail.com>
2023-08-30 23:53:56 +0900

    検索結果ページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "questions#index"
+  get '/search_genre', to: 'questions#search_genre'
   resources :questions do
     resources :answers, only: :create
   end
