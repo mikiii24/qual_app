32dc697e1dee9824adefc6b0ba93a5b9544ea1c6
mikiii24 <mikibask24@gmail.com>
2023-08-31 10:34:19 +0900

    ログイン状態での検索の許可

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :search_genre]
   def index
     @questions = Question.includes(:user).order('created_at DESC')
     @quals = Qual.all
